577082288778a166771db640df53235846c6d7a9
Matias DEVEZE <matias.deveze@etu.unilim.fr>
2023-11-28 15:31:35 +0100

    Update S01E02 main.php

--- common/S01E02/main.php
+++ common/S01E02/main.php
@@ -1,12 +1,31 @@
 <?php
-//Random array getting sorted
-$arraySorts = array_fill(0,999, 0);
-$array = array_map(random_int(0, 999), $arraySorts)
+$array = array_fill(0, 999, 0);
+$array = array_map(/**
+ * @throws Exception
+ */ function ($value) {
+    return random_int(0, 999);
+}, $array);
 ?>
-<div style="max-height: 500px; overflow : scroll;">
+<div style="max-height: 250px; overflow : scroll;">
     <pre>
         <?php
-            var_dump($array);
+        var_dump($array);
+        ?>
+    </pre>
+</div>
+<div style="max-height: 250px; overflow : scroll;">
+    <pre>
+        <?php
+        sort($array);
+        var_dump($array);
+        ?>
+    </pre>
+</div>
+<div style="max-height: 250px; overflow : scroll;">
+    <pre>
+        <?php
+        arsort($array);
+        var_dump($array);
         ?>
     </pre>
 </div>
