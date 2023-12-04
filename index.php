81f188e82d447442d79cd6eb37218deb20f551a7
Lucas <lucas.theodore2004@gmail.com>
2023-12-04 14:25:22 +0100

    Create index.php and main for operators

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+</head>
+
+<body>
+
+<h1>S01E01</h1>
+<a title="Tableau partie 1" href="/common/S01E01/Array/main.php">Tableau partie 1</a>
+<br>
+<a title="Fichier partie 1" href="/common/S01E01/File/main.php">Fichier partie 1</a>
+<br>
+<a title="Tableau partie 1" href="/common/S01E01/Operators/main.php">Fichier partie 1</a>
+<br>
+<h1>S01E02</h1>
+<br>
+
+<br>
+<h1>S01E03</h1>
+<br>
+
+<br>
+<h1>S01E04-5</h1>
+<br>
+
+
+</body>
+</html>
