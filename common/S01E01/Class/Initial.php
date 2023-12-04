af96794b2d8dd6fc1d69f7c635151fbe932b7e07
Lucas <lucas.theodore2004@gmail.com>
2023-12-04 15:12:20 +0100

    Le meilleur commit V2

--- common/S01E01/Class/Initial.php
+++ common/S01E01/Class/Initial.php
@@ -4,7 +4,21 @@ namespace Initial\S01E01;
 
 class Initial
 {
-    public function toCapitalLetter(){
+    function capitalLetter($arg)
+    {
+        echo strtoupper($arg);
+    }
+    
+    function escapeHTML($arg){
+        echo htmlspecialchars($arg, ENT_QUOTES, 'UTF-8');
+    }
 
-}
+    function convert($tab){
+        echo implode($tab);
+    }
+
+    function verifyEmail($arg)
+    {
+        echo "L'email est valide : ".filter_var($arg, FILTER_VALIDATE_EMAIL);
+    }
 }
