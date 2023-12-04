9d7f207c92045a6bf65e90011cab15492c1d8d9c
Lucas <lucas.theodore2004@gmail.com>
2023-12-04 15:30:49 +0100

    Changes by alexb

--- common/S01E01/Class/Initial.php
+++ common/S01E01/Class/Initial.php
@@ -2,7 +2,9 @@
 
 namespace Initial\S01E01;
 
-class Initial
+use Stringable;
+
+class Initial implements Stringable
 {
     function capitalLetter($arg)
     {
@@ -20,5 +22,39 @@ class Initial
     function verifyEmail($arg)
     {
         echo "L'email est valide : ".filter_var($arg, FILTER_VALIDATE_EMAIL);
+    }
+
+    function deleteLastCharacter($arg){
+        echo $arg;
+        array_slice($arg, end($arg));
+        echo $arg;
+    }
+
+    function mergeArray($arg1,$arg2){
+        echo array_merge($arg1,$arg2);
+    }
+
+
+    function printing(){
+        echo __FILE__;
+        echo __METHOD__;
+        echo __CLASS__;
+    }
+
+    function reverse($arg1){
+        echo strrev($arg1);
+    }
+
+    function __toString(): string
+    {
+        return self::class;
+    }
+
+    function lengthOfVariable($arg){
+        if (is_string($arg)){
+            return strlen($arg);
+        } else if (is_array($arg) || $arg instanceof Countable){
+            return count($arg);
+        }
     }
 }
