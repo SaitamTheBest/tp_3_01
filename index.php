c95f4cee9fc9037258c26ae4bea647cd1ba5637c
Lucas <lucas.theodore2004@gmail.com>
2023-12-17 17:47:24 +0100

    Perso LucasT

--- index.php
+++ index.php
@@ -20,7 +20,7 @@
 <h1>S01E02</h1>
 <a title="class partie 1" href="/common/S01E02/main.php">class partie 1</a>
 <br>
-<a title="redirection partie 1" href="/common/S01E02/Redirect.php">Redirection partie 1</a>
+<a title="redirection partie 1" href="/common/S01E02/Redirect.php">Redirection partie 1</a> //redirect_http.php ?
 <br>
 
 <br>
